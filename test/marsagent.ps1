f4fcc3180c4a67c3267e76f90dcd754b2cd0cc9b
Risto Lavett <risto.lavett@redeploy.se>
2016-02-23 17:04:39 +0100

    Update marsagent.ps1

--- test/marsagent.ps1
+++ test/marsagent.ps1
@@ -15,7 +15,7 @@ $credsfilename = Invoke-WebRequest -Uri $SplitCredStuff[0] -OutFile c:\temp\$($S
 $cred = $credspath + $credsfilename
 
 # Install Mars Agent
-c:\temp\MARSAgenInstaller.exe /q
+Start-Process c:\temp\MARSAgenInstaller.exe /q
 start-sleep -s 120
 import-module MSOnlineBackup
 # Register VM to Vault
