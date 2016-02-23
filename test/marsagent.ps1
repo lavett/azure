9eef8efa0dd9a54ca41f2a2a35cf4948deddbc06
Risto Lavett <risto.lavett@redeploy.se>
2016-02-23 12:28:14 +0100

    Update marsagent.ps1

--- test/marsagent.ps1
+++ test/marsagent.ps1
@@ -5,13 +5,45 @@ param (
         [string]
         $CredStuff
     )
-
-
+$CredsPath = "c:\temp\"
+$marsurl = "http://aka.ms/azurebackup_agent"
+$marsfile = "MARSAgentInstaller.exe"
+$marsagent = Invoke-WebRequest -Uri $marsurl | Out-File $CredsPath + $marsfile
 $CredsPath = "c:\temp\"
 mkdir c:\temp
 $SplitCredStuff = $CredStuff -split ';',2
-Invoke-WebRequest -Uri $SplitCredStuff[0] | Out-File c:\temp\$($SplitCredStuff[1])
+$credsfilename = Invoke-WebRequest -Uri $SplitCredStuff[0] | Out-File c:\temp\$($SplitCredStuff[1])
+$cred = $credspath + $credsfilename
 
+# Install Mars Agent
+$marsagent /q
+# Register VM to Vault
+Start-OBRegistration -VaultCredentials $cred -Confirm:$false
+# Set no proxy
+Set-OBMachineSetting -NoProxy
+# Se no bandwidth limit
+Set-OBMachineSetting -NoThrottle
+# Set Encryption Passphrase
+ConvertTo-SecureString -String "Complex!123_STRING" -AsPlainText -Force | Set-OBMachineSetting
+# Create new policy and schedule
+$newpolicy = New-OBPolicy
+$sched = New-OBSchedule -DaysofWeek Saturday, Sunday -TimesofDay 13:00
+Set-OBSchedule -Policy $newpolicy -Schedule $sched
+# Configure retention policy
+$retentionpolicy = New-OBRetentionPolicy -RetentionDays 7
+Set-OBRetentionPolicy -Policy $newpolicy -RetentionPolicy $retentionpolicy
+# Exclude and/or include files and directories
+$inclusions = New-OBFileSpec -FileSpec @("C:\", "D:\")
+$exclusions = New-OBFileSpec -FileSpec @("C:\windows", "C:\temp") -Exclude
+Add-OBFileSpec -Policy $newpolicy -FileSpec $inclusions
+Add-OBFileSpec -Policy $newpolicy -FileSpec $exclusions
+# Remove old policies of the VM and Apply the policy
+Get-OBPolicy | Remove-OBPolicy -Confirm:$false
+Set-OBPolicy -Policy $newpolicy -Confirm:$false
+# Start a manual backup 
+Get-OBPolicy | Start-OBBackup
+
+   
 
 
 
