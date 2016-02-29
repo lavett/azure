2cb9a4a9fdd84a80f396f00583c62396b58e9ab2
Risto Lavett <risto.lavett@redeploy.se>
2016-02-29 14:38:09 +0100

    commit

--- test/marsagent.ps1
+++ test/marsagent.ps1
@@ -3,48 +3,50 @@ param (
         [string]
         $CredStuff
     )
-$CredsPath = "c:\bvtemp\"
-mkdir $CredsPath
-$SplitCredStuff = $CredStuff -split ';',2
-$CredsFileName = Invoke-WebRequest -Uri $SplitCredStuff[0] -OutFile c:\bvtemp\$($SplitCredStuff[1])
-#$CredWriteFile = $CredStuff | Add-Content -Path c:\bvtemp\bvkey.VaultCredentials
-$CredFile = c:\bvtemp\$($SplitCredStuff[1])
-$cred = $credspath + $credsfilename
 
-Invoke-WebRequest -Uri http://aka.ms/azurebackup_agent -outfile c:\bvtemp\MARSAgentInstaller.exe
+		$CredsPath = "c:\bvtemp\"
+		$CredsName = "bvkey.VaultCredentials"
+		mkdir $CredsPath
+		$SplitCredStuff = $CredStuff -split ';',2
+		$GetCreds = Invoke-WebRequest -Uri $SplitCredStuff[0] -OutFile "$CredsPath$CredsName"
+		$CredsFileName = "$CredsPath$CredsName"
+		#$CredWriteFile = $CredStuff | Add-Content -Path c:\bvtemp\bvkey.VaultCredentials
+		#$CredFile = c:\bvtemp\$($SplitCredStuff[1])
 
-# Install Mars Agent
-Start-Process -FilePath "C:\bvtemp\MARSAgentInstaller.exe" -ArgumentList "/q"
-start-sleep 30
-import-module "C:\Program Files\Microsoft Azure Recovery Services Agent\bin\Modules\MSOnlineBackup"
-# Register VM to Vault
-Start-OBRegistration -VaultCredentials $cred -Confirm:$false
-# Set no proxy
-Set-OBMachineSetting -NoProxy
-# Se no bandwidth limit
-Set-OBMachineSetting -NoThrottle
-# Set Encryption Passphrase
-ConvertTo-SecureString -String "Complex!123_STRING" -AsPlainText -Force | Set-OBMachineSetting
-# Create new policy and schedule
-$newpolicy = New-OBPolicy
-$sched = New-OBSchedule -DaysofWeek Saturday, Sunday -TimesofDay 13:00
-Set-OBSchedule -Policy $newpolicy -Schedule $sched
-# Configure retention policy
-$retentionpolicy = New-OBRetentionPolicy -RetentionDays 7
-Set-OBRetentionPolicy -Policy $newpolicy -RetentionPolicy $retentionpolicy
-# Exclude and/or include files and directories
-$inclusions = New-OBFileSpec -FileSpec @("C:\", "D:\")
-$exclusions = New-OBFileSpec -FileSpec @("C:\windows", "C:\temp") -Exclude
-Add-OBFileSpec -Policy $newpolicy -FileSpec $inclusions
-Add-OBFileSpec -Policy $newpolicy -FileSpec $exclusions
-# Remove old policies of the VM and Apply the policy
-Get-OBPolicy | Remove-OBPolicy -Confirm:$false
-Set-OBPolicy -Policy $newpolicy -Confirm:$false
-# Start a manual backup
-Get-OBPolicy | Start-OBBackup
+		Invoke-WebRequest -Uri http://aka.ms/azurebackup_agent -outfile $CredsPath"MARSAgentInstaller.exe"
+
+		# Install Mars Agent
+		Start-Process -FilePath "C:\bvtemp\MARSAgentInstaller.exe" -ArgumentList "/q"
+		start-sleep 30
+		import-module "C:\Program Files\Microsoft Azure Recovery Services Agent\bin\Modules\MSOnlineBackup"
+		# Register VM to Vault
+		Start-OBRegistration -VaultCredentials $CredsFileName -Confirm:$false
+		# Set no proxy
+		Set-OBMachineSetting -NoProxy
+		# Se no bandwidth limit
+		Set-OBMachineSetting -NoThrottle
+		# Set Encryption Passphrase
+		ConvertTo-SecureString -String "Complex!123_STRING" -AsPlainText -Force | Set-OBMachineSetting
+		# Create new policy and schedule
+		$newpolicy = New-OBPolicy
+		$sched = New-OBSchedule -DaysofWeek Saturday, Sunday -TimesofDay 13:00
+		Set-OBSchedule -Policy $newpolicy -Schedule $sched
+		# Configure retention policy
+		$retentionpolicy = New-OBRetentionPolicy -RetentionDays 7
+		Set-OBRetentionPolicy -Policy $newpolicy -RetentionPolicy $retentionpolicy
+		# Exclude and/or include files and directories
+		$inclusions = New-OBFileSpec -FileSpec @("C:\", "D:\")
+		$exclusions = New-OBFileSpec -FileSpec @("C:\windows", "C:\temp") -Exclude
+		Add-OBFileSpec -Policy $newpolicy -FileSpec $inclusions
+		Add-OBFileSpec -Policy $newpolicy -FileSpec $exclusions
+		# Remove old policies of the VM and Apply the policy
+		Get-OBPolicy | Remove-OBPolicy -Confirm:$false
+		Set-OBPolicy -Policy $newpolicy -Confirm:$false
+		# Start a manual backup
+		Get-OBPolicy | Start-OBBackup
 
 
 
 
 
-#Remove-Item -Recurse -Force c:\bvtemp
+		#Remove-Item -Recurse -Force c:\bvtemp
