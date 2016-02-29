0b2be7a68aa76bf53198c802b737be0907cb418b
Risto Lavett <risto.lavett@redeploy.se>
2016-02-29 15:37:16 +0100

    commit

--- test/marsagent.ps1
+++ test/marsagent.ps1
@@ -9,6 +9,7 @@ param (
 		mkdir $CredsPath
 		$SplitCredStuff = $CredStuff -split ';',2
 		$GetCreds = Invoke-WebRequest -Uri $SplitCredStuff[0] -OutFile "$CredsPath$CredsName"
+		$BVString = $SplitCredStuff[2]
 		$CredsFileName = "$CredsPath$CredsName"
 		#$CredWriteFile = $CredStuff | Add-Content -Path c:\bvtemp\bvkey.VaultCredentials
 		#$CredFile = c:\bvtemp\$($SplitCredStuff[1])
@@ -26,13 +27,13 @@ param (
 		# Se no bandwidth limit
 		Set-OBMachineSetting -NoThrottle
 		# Set Encryption Passphrase
-		ConvertTo-SecureString -String "Complex!123_STRING" -AsPlainText -Force | Set-OBMachineSetting
+		ConvertTo-SecureString -String $BVString -AsPlainText -Force | Set-OBMachineSetting
 		# Create new policy and schedule
 		$newpolicy = New-OBPolicy
-		$sched = New-OBSchedule -DaysofWeek Saturday, Sunday -TimesofDay 13:00
+		$sched = New-OBSchedule -DaysofWeek Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday -TimesofDay 01:00
 		Set-OBSchedule -Policy $newpolicy -Schedule $sched
 		# Configure retention policy
-		$retentionpolicy = New-OBRetentionPolicy -RetentionDays 7
+		$retentionpolicy = New-OBRetentionPolicy -RetentionDays 31
 		Set-OBRetentionPolicy -Policy $newpolicy -RetentionPolicy $retentionpolicy
 		# Exclude and/or include files and directories
 		$inclusions = New-OBFileSpec -FileSpec @("C:\", "D:\")
@@ -43,5 +44,5 @@ param (
 		#Get-OBPolicy | Remove-OBPolicy -Confirm:$false
 		Set-OBPolicy -Policy $newpolicy -Confirm:$false
 		# Start a manual backup
-		Get-OBPolicy | Start-OBBackup
+		#Get-OBPolicy | Start-OBBackup
 		Remove-Item -Recurse -Force $CredsPath
